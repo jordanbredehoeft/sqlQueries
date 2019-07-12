bceb7026f34a4df93b57bf5bdba67ae8f424ecaa
jordanbrede <jbrede1@yahoo.com>
2019-07-12 10:01:00 -0700

    Added new query

--- TD_Dillards_SQLQueries.sql
+++ TD_Dillards_SQLQueries.sql
@@ -44,3 +44,28 @@ FROM
 	GROUP BY t.store,t_sales_year, t_month_num) AS monthly_trans;
 JOIN store_msa msa_hs ON daily_rev_trans.store = msa_hs.store
 GROUP BY msa_hs_grad_rate;
+
+/* Comparing AVG Daily Revenue of store with minimum local income vs maximum local income */
+
+SELECT msa.store, msa.city, msa.state, msa.msa_income,
+	SUM(monthly_trans.monthly_rev)/SUM(monthly_trans.numDates) AS AVG_Daily_Rev
+FROM 
+	(SELECT t.store, EXTRACT(MONTH FROM saledate) AS t_month_num, EXTRACT(year FROM saledate) AS t_sales_year, COUNT(DISTINCT saledate) AS numDates, SUM(amt) AS monthly_rev, (monthly_rev/COUNT(DISTINCT saledate)) AS avg_daily_rev
+	FROM trnsact t
+	JOIN (SELECT store, EXTRACT(year FROM saledate) AS sy, EXTRACT(MONTH FROM saledate) sm, 
+	CASE WHEN sm < 10 THEN sy||'0'||TRIM(sm)
+	                    ELSE sy||TRIM(sm)
+	                END AS ym
+	          FROM trnsact
+	         WHERE stype = 'P'
+	           AND ym <> 200508
+	         GROUP BY sy, sm, store, ym
+	        HAVING COUNT(DISTINCT saledate) >= 20) AS clean
+	ON t_sales_year = clean.sy
+	AND t_month_num = clean.sm
+	AND t.store = clean.store
+	WHERE stype ='P'
+	GROUP BY t.store,t_sales_year, t_month_num) AS monthly_trans
+JOIN store_msa msa ON monthly_trans.store = msa.store
+WHERE msa.msa_income IN ((SELECT MAX(msa_income) FROM store_msa),(SELECT MIN(msa_income) FROM store_msa))
+GROUP BY msa.store, msa.city, msa.state, msa.msa_income;
